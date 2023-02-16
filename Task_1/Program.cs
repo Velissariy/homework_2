fbc708ae45ef87f7c0fcb01ec51b039087c61797
Juno <polya.alexei@yandex.ru>
2023-02-16 08:26:19 +0300

    Сultivated the code

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Input the number");
 int x = Convert.ToInt32(Console.ReadLine()); 
 
 if ( x/100 == 0 || x/100 > 9)
-   Console.WriteLine("This number is  incorrect");
+   Console.WriteLine("This number is incorrect");
 else {
    int y = (x / 10)%10;
     Console.WriteLine(x+" "+"->"+" "+y);
