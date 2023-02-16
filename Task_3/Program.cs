12ea9b01607ae7c2f8cbcd47ceb855de1553215f
Juno <polya.alexei@yandex.ru>
2023-02-16 10:14:28 +0300

    Task_3 complited fnd added

---
+++ Task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+
+Console.WriteLine("Input the number");
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+switch (a) 
+{
+      case 1:
+           Console.WriteLine("1 -> нет"); //"Monday"
+           break;
+      case 2:
+           Console.WriteLine("2 -> нет"); //"Tuesday"
+           break;
+      case 3:
+           Console.WriteLine("3 -> нет"); //"Wednesday"
+           break;
+      case 4:
+           Console.WriteLine("4 -> нет"); //"Thursday"
+           break;
+      case 5:
+           Console.WriteLine("5 -> нет"); //"Friday"
+           break;
+      case 6:
+           Console.WriteLine("6 -> да"); //"Saturday"
+           break;
+      case 7:
+           Console.WriteLine("7 -> да");  //"Sunday"
+           break;
+      default:
+           Console.WriteLine("There is no such a weekday");
+           break;
+}
